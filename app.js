e1aeebb47c8434a0c36eca17999d72c803267116
Muhammad Nazran Mustaqim bin Abdul Rahm <Mustaqim>
2020-06-10 23:51:17 -0400

    added javascript folder, connected database. Need to include ejs in index.ejs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,6 +10,23 @@ app.use(bodyParser.urlencoded(
 ));
 
 app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin-mustaqim:Mustaqim98@cluster0-lbgkh.mongodb.net/productDB?retryWrites=true&w=majority", {useNewUrlParser: true});
+
+const productSchema = {
+    productid: String,
+    producturl: String,
+    producttitle: String,
+    productsubtitile: String,
+    productprice: String
+}
+
+const Man = mongoose.model("Man", productSchema);
+const Woman = mongoose.model("Woman", productSchema);
+const Home = mongoose.model("Home", productSchema);
+const Tech = mongoose.model("Tech", productSchema);
+const Lifestyle = mongoose.model("Lifestyle", productSchema);
+
 
 app.get("/", function (req, res) {
     res.render("index");
